849c00ed99e288196e0d01a8fae7b867c76a3621
weownthenight <Alexandrea@126.com>
2022-06-30 20:38:36 +0800

    update

--- baseline/transfertransfo/train.py
+++ baseline/transfertransfo/train.py
@@ -1,10 +1,23 @@
+import os
+import math
 import logging
 from pprint import pformat
 from argparse import ArgumentParser
+from collections import defaultdict
+from itertools import chain
 
 import torch
+from torch.nn.parallel import DistributedDataParallel
+from torch.utils.data import DataLoader, TensorDataset
+from ignite.engine import Engine, Events
+from ignite.handlers import ModelCheckpoint, global_step_from_engine
+from ignite.metrics import RunningAverage, Loss, MetricsLambda, Accuracy
+from ignite.contrib.handlers import ProgressBar, PiecewiseLinear
+from ignite.contrib.handlers.tensorboard_logger import TensorboardLogger, OutputHandler, OptimizerParamsHandler
 from transformers import (AdamW, OpenAIGPTDoubleHeadsModel, OpenAIGPTTokenizer,
-                          GPT2DoubleHeadsModel, GPT2Tokenizer)
+                          GPT2DoubleHeadsModel, GPT2Tokenizer, WEIGHTS_NAME, CONFIG_NAME)
+
+from utils import get_dataset, make_logdir
 
 # define special tokens
 SPECIAL_TOKENS = ["<bos>", "<eos>", "<speaker1>", "<speaker2>", "<pad>"]
@@ -12,8 +25,28 @@ SPECIAL_TOKENS = ["<bos>", "<eos>", "<speaker1>", "<speaker2>", "<pad>"]
 # https://huggingface.co/docs/transformers/v4.20.0/en/internal/tokenization_utils#transformers.SpecialTokensMixin.add_special_tokens
 ATTR_TO_SPECIAL_TOKENS = {'bos_token': '<bos>', 'eos_token': '<eos>', 'pad_token': '<pad>',
                           'additional_special_tokens': ['<speaker1>', '<speaker2>']}
+# except for mc_labels and n_candidates
+MODEL_INPUTS = ["input_ids", "mc_token_ids", "lm_labels", "mc_labels", "token_type_ids"]
+# parts of inputs which should be padded
+PADDED_INPUTS = ["input_ids", "lm_labels", "token_type_ids"]
 
 logger = logging.getLogger(__file__)
+
+def average_distributed_scalar(scalar, args):
+    """Average a scalar over the nodes if we are in distributed training. We use this for distributed evaluation."""
+    if args.local_rank == -1:
+        return scalar
+    scalar_t = torch.tensor(scalar, dtype=torch.float, device=args.device) / torch.distributed.get_world_size()
+    torch.distributed.all_reduce(scalar_t, op=torch.distributed.ReduceOp.SUM)
+    return scalar_t.item()
+
+def pad_dataset(dataset, padding=0):
+    """Pad the dataset. This could be optimized by defining a Dataset class and padding at the batch level, but this is simpler."""
+    max_l = max(len(x) for x in dataset["input_ids"])
+    for name in PADDED_INPUTS:
+        dataset[name] = [x + [padding if name != "lm_labels" else -100] * (max_l - len(x)) for x in dataset[name]]
+    return dataset
+
 
 def add_special_tokens_(model, tokenizer):
     """Add special tokens to the tokenizer and the model if they have not already been added."""
@@ -26,32 +59,121 @@ def add_special_tokens_(model, tokenizer):
     if num_added_tokens > 0:
         model.resize_token_embeddings(new_num_tokens=orig_num_tokens + num_added_tokens)
 
+
+def build_input_from_segments(persona, history, reply, tokenizer, lm_labels=False, with_eos=True):
+    """Build a sequence of input from segments: persona, history and last reply."""
+    bos, eos, speaker1, speaker2 = tokenizer.convert_tokens_to_ids(SPECIAL_TOKENS[:-1])
+    sequence = [[bos] + list(chain(*persona))] + history +[reply + ([eos] if with_eos else [])]
+    sequence = [sequence[0]] + [[speaker2 if (len(sequence)-i) % 2 else speaker1] + s for i, s in enumerate(sequence[1:])]
+    instance = {}
+    instance["input_ids"] = list(chain(*sequence))
+    # speaker1 or speaker2?
+    instance["token_type_ids"] = [speaker2 if i % 2 else speaker1 for i, s in enumerate(sequence) for _ in s]
+    instance["mc_token_ids"] = len(instance["input_ids"]) - 1
+    instance["lm_labels"] = [-100] * len(instance["input_ids"])
+    if lm_labels:
+        instance["lm_labels"] = ([-100] * sum(len(s) for s in sequence[:-1])) + [-100] + sequence[-1][1:]
+    return instance
+
+
 def get_data_loaders(args, tokenizer):
     """Prepare the dataset for training and evaluation"""
     personachat = get_dataset(tokenizer, args.dataset_path, args.dataset_cache)
+
+    logger.info("Build inputs and labels")
+    # when key does not exist, default(list) will return an empty list
+    datasets = {"train": defaultdict(list), "valid": defaultdict(list)}
+    # dataset_name:['train','valid']
+    for dataset_name, dataset in personachat.items():
+        num_candidates = len(dataset[0]["utterances"][0]["candidates"])
+        # we control the number of candidates in training set
+        # this is not for valid set, however
+        if args.num_candidates > 0 and dataset_name == 'train':
+            num_candidates = min(args.num_candidates, num_candidates)
+        for dialog in dataset:
+            persona = dialog["personality"].copy()
+            for _ in range(args.personality_permutations):
+                for utterance in dialog["utterances"]:
+                    history = utterance["history"][-(2*args.max_history+1):]
+                    # for every candidate, we build an input
+                    for j, candidate in enumerate(utterance["candidates"][-num_candidates:]):
+                        # only the last candidate is the real reply, this is a multiple choice problem
+                        lm_labels = bool(j == num_candidates-1)
+                        instance = build_input_from_segments(persona, history, candidate, tokenizer, lm_labels)
+                        for input_name, input_array in instance.items():
+                            datasets[dataset_name][input_name].append(input_array)
+                    # mc_labels is for multiple choice
+                    datasets[dataset_name]["mc_labels"].append(num_candidates - 1)
+                    # TODO: This can be put before the for loop and I will try
+                    datasets[dataset_name]["n_candidates"] = num_candidates
+                # permutated personalites
+                persona = [persona[-1]] + persona[:-1]
+
+    logger.info("Pad inputs and convert to Tensor")
+    tensor_datasets = {"train": [], "valid": []}
+    for dataset_name, dataset in datasets.items():
+        dataset = pad_dataset(dataset, padding=tokenizer.convert_tokens_to_ids(SPECIAL_TOKENS[-1]))
+        for input_name in MODEL_INPUTS:
+            tensor = torch.tensor(dataset[input_name])
+            if input_name != "mc_labels":
+                # after view, tensor has 3 dimensions, '+' means append here
+                # [262876, 285]-->[131438,2,285]
+                tensor = tensor.view((-1, datasets[dataset_name]["n_candidates"]) + tensor.shape[1:])
+            tensor_datasets[dataset_name].append(tensor)
+
+    logger.info("Build train and validation dataloaders")
+    train_dataset, valid_dataset = TensorDataset(*tensor_datasets["train"]), TensorDataset(*tensor_datasets["valid"])
+    train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset) if args.distributed else None
+    valid_sampler = torch.utils.data.distributed.DistributedSampler(valid_dataset) if args.distributed else None
+    train_loader = DataLoader(train_dataset, batch_size=args.train_batch_size, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=args.valid_batch_size, shuffle=False)
+
+    logger.info("Train dataset (Batch, Candidates, Seq length: {}".format(train_dataset.tensors[0].shape))
+    logger.info("Valid dataset (Batch, Candidates, Seq length: {}".format(valid_dataset.tensors[0].shape))
+    return train_loader, valid_loader, train_sampler, valid_sampler
 
 def train():
     parser = ArgumentParser()
     parser.add_argument("--dataset_path", type=str, default="", help="Path or url of the dataset. If empty download from S3.")
     parser.add_argument("--dataset_cache", type=str, default='./dataset_cache', help="Path or url of the dataset cache")
     parser.add_argument("--model_checkpoint", type=str, default="openai-gpt", help="Path, url or short name of the model")
+    parser.add_argument("--num_candidates", type=int, default=2, help="Number of candidates for training")
     # 这里建模的时候可以改变思路，不要直接的history作为input之一去建模，而是将对方给出的人格信息（印象）进行更新保留，自己已透露的人格进行记录
     # 可以先进行一个latent variable的embedding再送入transformer生成？
     parser.add_argument("--max_history", type=int, default=2, help="Number of previous exchanges to keep in history")
     parser.add_argument("--train_batch_size", type=int, default=4, help="Batch size for training")
     parser.add_argument("--valid_batch_size", type=int, default=4, help="Batch size for validation")
+    # NOTE: I am curious about the origin?
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=8, help="Accumulate gradients on several steps")
     # 这里default不是1e-5有点不理解？
     parser.add_argument("--lr", type=float, default=6.25e-5,help="Learning rate")
     parser.add_argument("--lm_coef", type=float, default=1.0, help="LM loss coefficient")
     parser.add_argument("--mc_coef", type=float, default=1.0, help="Multiple-choice loss coefficient")
+    parser.add_argument("--max_norm", type=float, default=1.0, help="Clipping gradient norm")
     parser.add_argument("--n_epochs", type=int, default=3, help="Number of training epochs")
+    # permutate the personality sentences
+    parser.add_argument("--personality_permutations", type=int, default=1, help="Number of permutations of personality sentences")
+    # --eval_before_start will trigger
+    parser.add_argument("--eval_before_start", action="store_true", help="If true start with a first evaluation before training")
     parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu", help="Device (cuda or cpu)")
+    # Though I don't have several GPUs, it is still need for learning to train the model distributedly
+    parser.add_argument("--local_rank", type=int, default=-1, help="Local rank for distributed training (-1: not distributed)")
     args = parser.parse_args()
 
-    # logging is set to INFO
-    logging.basicConfig(level=logging.INFO)
+    # logging is set to INFO (resp. WARN) for main (resp. auxiliary) process. logger.info => log main process only, logger.warning => log all process
+    logging.basicConfig(level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN)
+    # This is a logger warning: it will be printed by all distributed processes
+    logger.warning("Running process %d", args.local_rank)
     # pformat: return the formatted representation of object as a string.
     logger.info("Arguments: %s", pformat(args))
+
+    # Initialize distributed training if needed
+    # NOTE: I will research for distribute if I have time then
+    args.distributed = (args.local_rank != -1)
+    if args.distributed:
+        torch.cuda.set_device(args.local_rank)
+        args.device = torch.device("cuda", args.local_rank)
+        torch.distributed.init_process_group(backend="nccl", init_method='env://')
 
     logger.info("Prepare tokenizer, pretrained model and optimizer.")
     tokenizer_class = GPT2Tokenizer if "gpt2" in args.model_checkpoint else OpenAIGPTTokenizer
@@ -62,10 +184,117 @@ def train():
     model.to(args.device)
     # Add special tokens if they are not already added
     add_special_tokens_(model, tokenizer)
+    # TODO: I will research what optimizer is and how to use it
     optimizer = AdamW(model.parameters(), lr=args.lr, correct_bias=True)
+
+    # Prepare for distributed learning if needed, DistributedDataParallel is from torch.nn
+    if args.distributed:
+        model = DistributedDataParallel(model, device_ids=[args.local_rank], output_device=args.local_rank)
 
     logger.info("Prepare datasets")
     train_loader, val_loader, train_sampler, valid_sampler = get_data_loaders(args, tokenizer)
+
+    # Training function and trainer
+    def update(engine, batch):
+        # convert to train mode
+        model.train()
+        # move to cuda
+        batch = tuple(input_tensor.to(args.device) for input_tensor in batch)
+        input_ids, mc_token_ids, lm_labels, mc_labels, token_type_ids = batch
+        # In the latest transformers, the parameter "labels" stands for "lm_labels"
+        outputs = model(
+            input_ids, token_type_ids=token_type_ids, mc_token_ids=mc_token_ids,
+            mc_labels=mc_labels, labels=lm_labels
+        )
+        loss = (outputs.loss * args.lm_coef + outputs.mc_loss * args.mc_coef) / args.gradient_accumulation_steps
+        loss.backward()
+        # NOTE: why we should do this? The strength is?
+        torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_norm)
+        if engine.state.iteration % args.gradient_accumulation_steps == 0:
+            optimizer.step()
+            optimizer.zero_grad()
+        return loss
+
+    # NOTE: I need to learn about PyTorch Ignite
+    trainer = Engine(update)
+
+    # Evaluation function and evaluator (evaluator output is the input of the metrics)
+    def inference(engine, batch):
+        model.eval()
+        with torch.no_grad():
+            batch = tuple(input_tensor.to(args.device) for input_tensor in batch)
+            input_ids, mc_token_ids, lm_labels, mc_labels, token_type_ids = batch
+            logger.info(tokenizer.decode(input_ids[0, -1, :].tolist()))
+            # if we don't send labels to model, it doesn't return losses
+            outputs = model(
+                input_ids, token_type_ids=token_type_ids, mc_token_ids=mc_token_ids,
+            )
+            lm_logits_flat_shifted = outputs.logits[..., :-1, :].contiguous().view(-1, outputs.logits.size(-1))
+            lm_labels_flat_shifted = lm_labels[..., 1:].contiguous().view(-1)
+            return (lm_logits_flat_shifted, outputs.mc_logits), (lm_labels_flat_shifted,mc_labels)
+
+    evaluator = Engine(inference)
+
+    # Attach evaluation to trainer: we evaluate when we start the training and at the end of each epoch
+    trainer.add_event_handler(Events.EPOCH_COMPLETED, lambda _: evaluator.run(val_loader))
+    if args.n_epochs < 1:
+        trainer.add_event_handler(Events.COMPLETED, lambda _: evaluator.run(val_loader))
+    if args.eval_before_start:
+        trainer.add_event_handler(Events.STARTED, lambda _: evaluator.run(val_loader))
+
+    # Make sure distributed data samplers split the dataset nicely between the distributed processes
+    # NOTE: distributed learning
+    if args.distributed:
+        trainer.add_event_handler(Events.EPOCH_STARTED, lambda engine: train_sampler.set_epoch(engine.state.epoch))
+        evaluator.add_event_handler(Events.EPOCH_STARTED, lambda engine: valid_sampler.set_epoch(engine.state.epoch))
+
+    # Linearly decrease the learning rate from lr to zero
+    # NOTE: PiecewiseLinear is from ignite
+    schedular = PiecewiseLinear(optimizer, "lr", [(0, args.lr), (args.n_epochs * len(train_loader), 0.0)])
+    trainer.add_event_handler(Events.ITERATION_STARTED, schedular)
+
+    # Prepare metrics - note how we compute distributed metrics
+    # NOTE: RunningAverage, Average, Loss, MetricsLambda is from ignite
+    RunningAverage(output_transform=lambda x: x).attach(trainer, "loss")
+    metrics = {"nll": Loss(torch.nn.CrossEntropyLoss(ignore_index=-100), output_transform=lambda x: (x[0][0], x[1][0])),
+                   "accuracy": Accuracy(output_transform=lambda x: (x[0][1], x[1][1]))}
+    # distribute
+    metrics.update({"average_nll": MetricsLambda(average_distributed_scalar, metrics['nll'], args),
+                        "average_accuracy": MetricsLambda(average_distributed_scalar, metrics["accuracy"], args)})
+    metrics["average_ppl"] = MetricsLambda(math.exp, metrics["average_nll"])
+    for name, metric in metrics.items():
+        metric.attach(evaluator, name)
+
+    # On the main process: add progress bar, tensorboard, checkpoints and save model, configuration and tokenizer before we start to train
+    if args.local_rank in [-1, 0]:
+        pbar = ProgressBar(persist=True)
+        pbar.attach(trainer, metric_names=["loss"])
+        evaluator.add_event_handler(Events.COMPLETED, lambda _: pbar.log_message("Validation: %s" % pformat(evaluator.state.metrics)))
+
+        log_dir = make_logdir(args.model_checkpoint)
+        tb_logger = TensorboardLogger(log_dir)
+
+        tb_logger.attach(trainer, log_handler=OutputHandler(tag="training", metric_names=["loss"]), event_name=Events.ITERATION_COMPLETED)
+        tb_logger.attach(trainer, log_handler=OptimizerParamsHandler(optimizer), event_name=Events.ITERATION_STARTED)
+        tb_logger.attach(evaluator, log_handler=OutputHandler(tag="validation", metric_names=list(metrics.keys()), global_step_transform=global_step_from_engine(trainer)), event_name=Events.EPOCH_COMPLETED)
+
+        checkpoint_handler = ModelCheckpoint(log_dir, 'checkpoint', save_interval=1, n_saved=3)
+        # "getattr" takes care of distributed encapsulation
+        trainer.add_event_handler(Events.EPOCH_COMPLETED, checkpoint_handler, {'mymodel': getattr(model, 'module', model)})
+
+        torch.save(args, log_dir + '/model_training_args.bin')
+        # NOTE: what is CONFIG_NAME for? it is from transformers
+        getattr(model, 'module', model).config.to_json_file(os.path.join(log_dir, CONFIG_NAME))
+        tokenizer.save_pretrained(log_dir)
+
+    # Run the training
+    trainer.run(train_loader, max_epochs=args.n_epochs)
+
+    # On the main process: close tensorboard logger and rename the last checkpoint (for easy re-loading with OpenAIGPTModel.from_pretrained method)
+    if args.local_rank in [-1, 0] and args.n_epochs > 0:
+        # TODO: PR in ignite to have better access to saved file paths (cleaner)
+        os.rename(os.path.join(log_dir, checkpoint_handler._saved[-1][1]), os.path.join(log_dir, WEIGHTS_NAME))
+        tb_logger.close()
 
 if __name__ == "__main__":
     train()
--- baseline/transfertransfo/utils.py
+++ baseline/transfertransfo/utils.py
@@ -1,6 +1,53 @@
-PERSONA_URL = "https://s3.amazonaws.com/datasets.huggingface.co/personachat/personachat_self_original.json"
+from datetime import datetime
+import logging
+import os
+import json
+import socket
+
+import torch
+
+from transformers import cached_path
+
+# TODO: use original txt file to convert to the json file format with the features we like
+PERSONACHAT_URL = "https://s3.amazonaws.com/datasets.huggingface.co/personachat/personachat_self_original.json"
+
+logger = logging.getLogger(__file__)
+
 
 def get_dataset(tokenizer, dataset_path, dataset_cache):
     """Get tokenized PERSONACHAT dataset from S3 or cache."""
+    # if it is dataset_path, then the dataset is not tokenized
     dataset_path = dataset_path or PERSONACHAT_URL
-    dataset_cache =
+    # To avoid using GPT cache for GPT-2 and vice versa
+    dataset_cache = dataset_cache + '_' + type(tokenizer).__name__
+    if dataset_cache and os.path.isfile(dataset_cache):
+        logger.info("Load tokenized dataset from cache at %s", dataset_cache)
+        # Loads an object saved with torch.save() from a file.
+        dataset = torch.load(dataset_cache)
+    else:
+        logger.info("Download dataset from %s", dataset_path)
+        personachat_file = cached_path(dataset_path)
+        with open(personachat_file, "r", encoding="utf-8") as f:
+            dataset = json.loads(f.read())
+
+        logger.info("Tokenize and encode the dataset")
+        def tokenize(obj):
+            if isinstance(obj, str):
+                return tokenizer.convert_tokens_to_ids(tokenizer.tokenize(obj))
+            if isinstance(obj, dict):
+                return dict((n, tokenize(o)) for n, o in obj.items())
+            return list(tokenize(o) for o in obj)
+        dataset = tokenize(dataset)
+        torch.save(dataset, dataset_cache)
+    return dataset
+
+
+# After Python 3.5, you can assign the parameter's type, if 'model_name' is not a str, you will get a warning.
+def make_logdir(model_name: str):
+    """Create unique path to save results and checkpoints, e.g. runs/Sep22_19-45-59_gpu-7_gpt2"""
+    # Code copied from ignite repo
+    current_time = datetime.now().strftime('%b%d_%H-%M-%S')
+    # NOTE: socket?
+    logdir = os.path.join(
+        'runs', current_time + '_' + socket.gethostname() + '_' + model_name)
+    return logdir
